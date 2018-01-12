2f634f0e00600ddd12eed960e62d700fb9dffd9e
A283le <alexdominguez169@gmail.com>
2018-01-12 11:22:00 -0500

    Started net

---
+++ software/src/network.cpp
@@ -0,0 +1,16 @@
+#include "network.h"
+#include <iostream>
+
+using namespace std;
+
+Net::Net(){
+   return 0;
+}
+
+bool Net::rand_node(){
+
+}
+
+bool Net::rand_connection(){
+
+}
---
+++ software/src/network.h
@@ -0,0 +1,15 @@
+#ifdef
+#ifndef  // this iffy
+
+class Net
+{
+   private:
+   protected:
+      int **nodes;
+   public:
+      Net();
+      bool rand_node();
+      bool rand_connection();
+};
+
+#endif
