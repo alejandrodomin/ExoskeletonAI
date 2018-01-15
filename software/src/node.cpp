57d803541f6a829c55ba30af7c39cd2cb737dd1f
erami077 <ramirezeliel@yahoo.com>
2018-01-15 13:43:43 -0500

    started node class

---
+++ software/src/node.cpp
@@ -0,0 +1,19 @@
+#include "node.h"
+#include <iostream>
+
+using namespace std;
+
+Node::Node()
+{
+
+}
+
+Node::~Node()
+{
+
+}
+
+void Node::out_func(Node *inputs, float *weights_input, float bias)
+{
+    
+}
---
+++ software/src/node.h
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+#ifndef NODE_H
+#define NODE_H
+
+class Node: public Network
+{
+    private:
+        Node *inputs;
+        Node *outputs;
+        float output_func;
+        float *weights_input;
+        float bias;
+
+    public:
+        Node();
+        ~Node();
+
+        void out_func(Node *, float *, float);
+
+};
+#endif
