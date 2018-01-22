877ce6ea160f1f75835a95aa98d9df69439768a6
erami077 <ramirezeliel@yahoo.com>
2018-01-22 13:14:44 -0500

    updates

--- software/include/output.h
+++ software/include/output.h
@@ -8,6 +8,9 @@ using namespace std;
 class Output: public Node 
 {
 public:
+    Output();
+   ~Output();
+    Node** inputs;
     virtual void add_input(Node *node);
     virtual void add_output(Node *node);
 };
