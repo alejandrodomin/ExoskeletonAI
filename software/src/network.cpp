286f7fe1830a7bf8d10874ec3a7ff442c0fc270f
A283le <alexdominguez169@gmail.com>
2018-02-06 20:28:07 -0500

    Added some cout statements for debuging.

--- software/src/network.cpp
+++ software/src/network.cpp
@@ -70,10 +70,12 @@ void Network::input_run(){
 }
 
 void Network::hidden_run(){
-
+    cout << "[INFO][NETWORK]: Entered Network::hidden_run()." << endl;
+    cout << "[INFO][NETWORK]: Exiting Network::hidden_run()." << endl;
 }
 
 void Network::output_run(){
+    cout << "[INFO][NETWORK]: Entered Network::output_run()." << endl;
     for (int index = 0; index < NUM_OUTPUTS; index++){
         threads[index] = out_nodes[index]->spawn_thread();
     }
@@ -91,41 +93,56 @@ void Network::output_run(){
         delete [] threads;
         threads = NULL;
     }
+    cout << "[INFO][NETWORK]: Exiting Network::output_run()." << endl;
 }
 
 void Network::mutate(){
+    cout << "[INFO][NETWORK]: Entered Network::mutate()." << endl;
     int num = (rand() % 3) + 1;
 
     if(num % 3 == 0)
         rand_node();
     else rand_connection();
+    cout << "[INFO][NETWORK]: Exiting Network::mutate()." << endl;
 }
 
 void Network::use_output(){
-
+    cout << "[INFO][NETWORK]: Entered Network::use_output()." << endl;
+    cout << "[INFO][NETWORK]: Exiting Network::use_output()." << endl;
 }
 
 void Network::set_num_nodes(int num_nodes){
+    cout << "[INFO][NETWORK]: Entered Network::set_num_nodes(int)." << endl;
     this->num_nodes = num_nodes;
+    cout << "[INFO][NETWORK]: Exiting Network::set_num_nodes(int)." << endl;
 }
 
 void Network::set_compatibility_distance(float newcomp_distance){
+    cout << "[INFO][NETWORK]: Entered Network::set_compatibility_distance(float)." << endl;
     compatibility_distance = newcomp_distance;
+    cout << "[INFO][NETWORK]: Exiting Network::set_compatibility_distance(float)." << endl;
 }
 
 int Network::get_fitness(){
+    cout << "[INFO][NETWORK]: Entered Network::get_fitness()." << endl;
+    cout << "[INFO][NETWORK]: Exiting Network::get_fitness()." << endl;
     return fitness;
 }
 
 int Network::get_num_nodes(){
+    cout << "[INFO][NETWORK]: Entered Network::get_num_nodes()." << endl;
+    cout << "[INFO][NETWORK]: Exiting Network::get_num_nodes()." << endl;
     return num_nodes;
 }
 
 bool Network::rand_node(){
+    cout << "[INFO][NETWORK]: Entered Network::rand_node()." << endl;
     hidden_nodes.push_back(new Node(hidden));
+    cout << "[INFO][NETWORK]: Exiting Network::rand_node()." << endl;
 }
 
 bool Network::rand_connection(){
+    cout << "[INFO][NETWORK]: Entered Network::rand_connection()." << endl;
    int node_one = rand()% num_nodes;
    int node_two = rand()% num_nodes;
 
@@ -144,9 +161,11 @@ bool Network::rand_connection(){
    }
 
    one->add_gene(one, two);
+   cout << "[INFO][NETWORK]: Exiting Network::rand_connection()." << endl;
 }
 
 bool Network::add_input_node(int num_nodes){
+    cout << "[INFO][NETWORK]: Entered Network::add_input_node(int)." << endl;
     int index = 0;
     while(num_nodes > 0){
         in_nodes[index] = new Node(input);
@@ -155,10 +174,12 @@ bool Network::add_input_node(int num_nodes){
         index++;
     }
 
+    cout << "[INFO][NETWORK]: Exiting Network::add_input_node(int)." << endl;
     return true;
 }
 
 bool Network::add_output_nodes(int num_nodes){
+    cout << "[INFO][NETWORK]: Entered Network::add_output_nodes(int)." << endl;
     int index = 0;
     while(num_nodes > 0){
         out_nodes[index] = new Node(output);
@@ -167,13 +188,17 @@ bool Network::add_output_nodes(int num_nodes){
         index++;
     }
 
+    cout << "[INFO][NETWORK]: Exiting Network::add_output_nodes(int)." << endl;
     return true;
 }
 
 Node** Network::get_input(){
+    cout << "[INFO][NETWORK]: Entered Network::get_input()." << endl;
+    cout << "[INFO][NETWORK]: Exiting Network::get_input()." << endl;
     return in_nodes;
 }
 
 list<Network*> Network::reproduce(){
-
+    cout << "[INFO][NETWORK]: Entered Network::reproduce()." << endl;
+    cout << "[INFO][NETWORK]: Exiting Network::reproduce()." << endl;
 }
