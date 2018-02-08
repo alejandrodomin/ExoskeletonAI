3fbe50831f991d1c164db4a3df759f8634bcf6bb
A283le <alexdominguez169@gmail.com>
2018-02-08 16:43:36 -0500

    Global variables go here.

---
+++ software/include/common.hpp
@@ -0,0 +1,8 @@
+#ifndef COMMON_HPP
+#define COMMON_HPP
+
+static int global_innovation_id = 0; // this shit needs to be in the network.
+					// every network need to have their own global
+					// innovation id, shits wrong.
+
+#endif
