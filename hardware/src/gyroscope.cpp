ba506357bba6447eb5e0385cb5b39f1242efe3c0
Jordan Ashton <jor.ashton@gmail.com>
2018-03-09 12:25:30 -0500

    modified gyroscope.cpp to test new methods

--- hardware/src/gyroscope.cpp
+++ hardware/src/gyroscope.cpp
@@ -7,7 +7,7 @@
 #include "LSM9DS1_Types.h"
 #include "LSM9DS1.h"
 
-int main(int argc, char *argv[]) {
+/*int main(int argc, char *argv[]) {
     LSM9DS1 imu(IMU_MODE_I2C, 0x6a, 0x1c);
     imu.begin();
     if (!imu.begin()) {
@@ -31,4 +31,43 @@ int main(int argc, char *argv[]) {
     }
 
     exit(EXIT_SUCCESS);
+}*/
+
+void gyroInit(float *gyro, float *accel, float *mag);
+
+int main (int argc, char **argv){
+	float a[3] = {0}, b[3] = {0}, c[3] = {0};
+	gyroInit(a,b,c);
+	printf("%f, %f, %f [deg/s]\n", a[0], a[1], a[2]);
+
+exit(0);
+}
+
+void gyroInit(float *gyro, float *accel, float *mag){
+	float *loc = NULL;
+
+	LSM9DS1 imu(IMU_MODE_I2C, 0x6a, 0x1c);
+	imu.begin();
+	if (!imu.begin()){
+		fprintf(stderr, "Failed to communicate with LSM9DS1.\n");
+		exit(EXIT_FAILURE);
+	}
+	imu.calibrate();
+
+	for(;;){
+		while (!imu.gyroAvailable());
+		imu.readGyro();
+		while (!imu.accelAvailable());
+		imu.readAccel();
+		while (!imu.magAvailable());
+		imu.readMag();
+
+		*gyro = imu.calcGyro(imu.gx);
+		*loc = *gyro + 1;
+		*loc = imu.calcGyro(imu.gy);
+		*loc = *gyro + 2;
+		*loc = imu.calcGyro(imu.gz);
+		sleep(1.0);
+	}
+	exit(EXIT_SUCCESS);
 }
