d3c9a6f1e17a4e838292ead2b6a8e8cf8ede12fa
A283le <alexdominguez169@gmail.com>
2018-03-25 21:04:59 -0400

    Added boost library to the setup.bash

--- setup.bash
+++ setup.bash
@@ -11,31 +11,34 @@ elif [ "$CONT" = "y" ]; then
 		echo "export PATH=$PATH:$PWD/software/include" >> ~/.bashrc
 	fi
 
-	sudo apt-get update
+	sudo apt update
 
 	read -p "Would you like to install VSCode: (y/n)" ANSW
 	if [ "$ANSW" = "y" ]; then
-		sudo apt-get install snapd-xdg-open
+		sudo apt install snapd-xdg-open
 		sudo snap install vscode --classic
 	else
 		echo "Aborting instalation of VSCode."
 	fi
 
+	echo "Installing Boost C++ Library"
+	sudo apt install libboost-all-dev
+
 	echo "Installing MPI."
-	sudo apt-get install openmpi-bin openmpi-common openssh-client openssh-server libopenmpi1.10 libopenmpi-dev
+	sudo apt install openmpi-bin openmpi-common openssh-client openssh-server libopenmpi1.10 libopenmpi-dev
 
 	echo "Installing Doxygen."
-	sudo apt-get install doxygen
+	sudo apt install doxygen
 
 	echo "Installing Google Test support."
-	sudo apt-get install libgtest-dev
-	sudo apt-get install cmake
+	sudo apt install libgtest-dev
+	sudo apt install cmake
 	cd /usr/src/gtest
 	sudo cmake CMakeLists.txt
 	sudo make
 	sudo cp *.a /usr/lib
 
-#	sudo reboot
+	sudo reboot
 else
 	echo "Command not recognized, aborting setup."
 fi
