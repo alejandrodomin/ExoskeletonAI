64bed2cc5fe8a1d06041f76c8aa58e343da2e7ac
Jordan Ashton <jor.ashton@gmail.com>
2018-03-28 18:15:22 -0400

    edited gyroscope.c to fascilitate 2 imu's connected to 1 rbpi

--- hardware/src/gyroscope.cpp
+++ hardware/src/gyroscope.cpp
@@ -35,7 +35,7 @@ int main(int argc, char *argv[]) {
 }
 */
 
-LSM9DS1 imuInit(float* gyro, float* accel, float* mag, int imuNum){
+LSM9DS1 imuInit(int imuNum){
 
 	if (imuNum == 0){
 		LSM9DS1 imu(IMU_MODE_I2C, 0x6a, 0x1c);
@@ -46,24 +46,16 @@ LSM9DS1 imuInit(float* gyro, float* accel, float* mag, int imuNum){
 		}
 	}
 	else if (imuNum == 1){
-		LSM9DS1 imu(IMU_MODE_I2C, 0x6a, 0x1c);
+		LSM9DS1 imu(IMU_MODE_I2C, 0x6b, 0x1e);
 		imu.begin();
 		if (!imu.begin()){
 			fprintf(stderr, "Failed to communicate with LSM9DS1.\n");
 			exit(EXIT_FAILURE);
 		}
 	}
-	else if (imuNum == 2){
-		LSM9DS1 imu(IMU_MODE_I2C, 0x6a, 0x1c);
-		imu.begin();
-		if (!imu.begin()){
-			fprintf(stderr, "Failed to communicate with LSM9DS1.\n");
-			exit(EXIT_FAILURE);
-		}
-	}
-	else
+	else{
 		fprintf(stderr, "Incorrect imu choice, chose 0,1,2.\n");
-
+	}
 	imu.calibrate();
 	return imu;
 }
@@ -101,18 +93,19 @@ void imuRead(float* gyro, float* accel, float* mag, LSM9DS1 imu){
 
 int main (int argc, char **argv){
 	float g[3] = {0}, a[3] = {0}, m[3] = {0};
-	LSM9DS1 imu1 = imuInit(g,a,m,0);
+	LSM9DS1 imu1 = imuInit(0);
+	LSM9DS1 imu2 = imuInit(1);
 	imuRead(g,a,m,imu1);
 	printf("%f, %f, %f [deg/s]\n", g[0], g[1], g[2]);
 	printf("%f, %f, %f [Gs]\n", a[0], a[1], a[2]);
 	printf("%f, %f, %f [gauss]\n", m[0], m[1], m[2]);
 	
-	imuRead(g,a,m,imu);
+	imuRead(g,a,m,imu2);
 	printf("%f, %f, %f [deg/s]\n", g[0], g[1], g[2]);
 	printf("%f, %f, %f [Gs]\n", a[0], a[1], a[2]);
 	printf("%f, %f, %f [gauss]\n", m[0], m[1], m[2]);
 
-	imuRead(g,a,m,imu);
+	imuRead(g,a,m,imu1);
 	printf("%f, %f, %f [deg/s]\n", g[0], g[1], g[2]);
 	printf("%f, %f, %f [Gs]\n", a[0], a[1], a[2]);
 	printf("%f, %f, %f [gauss]\n", m[0], m[1], m[2]);
